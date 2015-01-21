0456ea1643c37ec88979250a1ddca8ed85721f4c
sovlin <vincentherl@live.fr>
2015-01-21 14:10:13 +0100

    First test

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+GRAILS_HOME=/lib/grails
+export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+export PATH="$PATH:$JAVA_HOME/bin"
+export PATH="$PATH:$GRAILS_HOME/bin"
+grails $WERCKER_STEP_GRAILS_OPTIONS
