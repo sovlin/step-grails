056abb0d53f2619a69466bd1288ff6900462c5a0
sovlin <vincentherl@live.fr>
2015-01-21 14:31:27 +0100

    Remove step in options

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ GRAILS_HOME=/lib/grails
 export JAVA_HOME=/usr/lib/jvm/java-8-oracle
 export PATH="$PATH:$JAVA_HOME/bin"
 export PATH="$PATH:$GRAILS_HOME/bin"
-grails $WERCKER_STEP_GRAILS_OPTIONS
+grails $WERCKER_GRAILS_OPTIONS
