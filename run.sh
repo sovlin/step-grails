91341d4fc098c1d75166e7b0c65d6f037515bace
sovlin <vincentherl@live.fr>
2015-01-30 10:07:05 +0100

    add version system

--- run.sh
+++ run.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-if [ $WERCKER_GRAILS_VERSION -eq "snapshot" ]
+if [ $WERCKER_GRAILS_VERSION ]
 then
-  GRAILS_HOME=/lib/grails-snapshot
+    if [ $WERCKER_GRAILS_VERSION -eq "2.4.4" ]
+    then
+      GRAILS_HOME=/lib/grails-snapshot
+    elif [ $WERCKER_GRAILS_VERSION -eq "3.0.0.M1" ]
+    then
+      GRAILS_HOME=/lib/grails
+    else
+      echo "unknown version:$WERCKER_GRAILS_VERSION"
+    fi
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    export PATH="$PATH:$JAVA_HOME/bin"
+    export PATH="$PATH:$GRAILS_HOME/bin"
+    grails $WERCKER_GRAILS_OPTIONS
 else
-  GRAILS_HOME=/lib/grails
+    echo "version:<not set>"
 fi
-export JAVA_HOME=/usr/lib/jvm/java-8-oracle
-export PATH="$PATH:$JAVA_HOME/bin"
-export PATH="$PATH:$GRAILS_HOME/bin"
-grails $WERCKER_GRAILS_OPTIONS
