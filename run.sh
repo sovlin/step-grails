2557a81a9da90f59632b432666913860931712b8
sovlin <vincentherl@live.fr>
2015-02-07 15:26:46 +0100

    M2 test

--- run.sh
+++ run.sh
@@ -4,15 +4,10 @@ then
     case "$WERCKER_GRAILS_VERSION" in
         "2.4.4" )
             GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION;;
-        "3.0.0.M2" )
-            if [ $WERCKER_GRAILS_COMMIT ]
-            then
-                source ./grails-snapshot-run.sh
-            else
-                echo "commit:<not set>"
-            fi;;
         "3.0.0.M1" )
             GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION;;
+        "3.0.0.M2" )
+            echo "MD2";;
         *)
             echo "unknown version:$WERCKER_GRAILS_VERSION"
     esac
