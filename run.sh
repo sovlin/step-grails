f41752127cd6a6f96b7d963956e5c2cf6d3dfae4
sovlin <vincentherl@live.fr>
2015-02-07 15:05:01 +0100

    add case

--- run.sh
+++ run.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 if [ $WERCKER_GRAILS_VERSION ]
 then
-    if [ "$WERCKER_GRAILS_VERSION" == "2.4.4" ]
-    then
-      GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION
-    elif [ "$WERCKER_GRAILS_VERSION" == "3.0.0.M1" ]
-    then
-      GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION
-    elif [ "$WERCKER_GRAILS_VERSION" == "3.0.0.BUILD-SNAPSHOT" ]
-    then
-      if [ $WERCKER_GRAILS_COMMIT ]
-      then
-        source ./grails-snapshot-run.sh
-      else
-        echo "commit:<not set>"
-      fi
-    else
-      echo "unknown version:$WERCKER_GRAILS_VERSION"
-    fi
+    case "$WERCKER_GRAILS_VERSION" in
+        "2.4.4" )
+            GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION;;
+        "3.0.0.M1" )
+            GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION;;
+        "3.0.0.BUILD-SNAPSHOT" )
+            if [ $WERCKER_GRAILS_COMMIT ]
+            then
+                source ./grails-snapshot-run.sh
+            else
+                echo "commit:<not set>"
+            fi;;
+        *)
+            echo "unknown version:$WERCKER_GRAILS_VERSION"
+    esac
     export JAVA_HOME=/usr/lib/jvm/java-8-oracle
     export PATH="$PATH:$JAVA_HOME/bin"
     export PATH="$PATH:$GRAILS_HOME/bin"
