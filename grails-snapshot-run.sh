81a17a036c838092a2eb87368eb0200bd814c0a8
sovlin <vincentherl@live.fr>
2015-02-07 14:28:50 +0100

    add grails-snapshot

---
+++ grails-snapshot-run.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+WERCKER_GRAILS_COMMIT=""
+if [ "$WERCKER_GRAILS_COMMIT" != "" ]
+then
+ echo "parent -> run.sh" 
+else
+ WERCKER_GRAILS_COMMIT=721dba7b99de0f092a228bf5ca226ffc82ef261f
+fi
+mkdir target
+cd target
+echo "--- GIT CLONE & INSTALL GRAILS $(pwd) ---"
+# git clone https://github.com/grails/grails-core.git ./
+git clone https://github.com/sovlin/grails-core.git ./
+echo "--- CHANGE COMMIT $WERCKER_GRAILS_COMMIT ---"
+# mv ./grails-core/* ./
+# rm -r ./grails-core
+git show $WERCKER_GRAILS_COMMIT
+git checkout $WERCKER_GRAILS_COMMIT
+./gradlew install
+GRAILS_HOME=$(pwd)"/bin"
+echo "GRAILS_HOME=$GRAILS_HOME"
--- run.sh
+++ run.sh
@@ -7,6 +7,14 @@ then
     elif [ "$WERCKER_GRAILS_VERSION" == "3.0.0.M1" ]
     then
       GRAILS_HOME=/lib/grails-$WERCKER_GRAILS_VERSION
+    elif [ "$WERCKER_GRAILS_VERSION" == "3.0.0.BUILD-SNAPSHOT" ]
+    then
+      if [ $WERCKER_GRAILS_COMMIT ]
+      then
+        source ./grails-snapshot-run.sh
+      else
+        echo "commit:<not set>"
+      fi
     else
       echo "unknown version:$WERCKER_GRAILS_VERSION"
     fi
